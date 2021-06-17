ac14a74811cd027b7dd8762f7a05e31eba3ee492
musmuliadi08 <musliadics12@gmail.com>
2021-06-18 01:50:42 +0800

    mengubah tampilan users dosen&mhs

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-var navLink = document.querySelectorAll('.nav-link');
-var btnUser = document.querySelector('.btn-user');
 var btnMasuk = document.querySelector('.btn-masuk');
-var btnBuatAkun = document.querySelector('.btn-buat-akun');
-
 
 if(localStorage.getItem('theme') == 'tDark')
 	setDarkMode()
@@ -16,33 +12,9 @@ function setDarkMode() {
 		iconDarkMode = '<h4><i class="bi bi-brightness-high-fill"></i></h4>';
 		localStorage.setItem('theme', 'tDark');
 
-		// for (var i = 0; i <= navLink.length; i++) {
-		// 	navLink[i].classList.toggle('text-white');
-		// }
-
-		// btnUser.classList.remove('bg-light');
-		// btnUser.classList.add('bg-secondary');		
-
-		 // btnMasuk.classList.toggle('btn-secondary');
-
-		// btnBuatAkun.classList.remove('bg-light');
-		// btnBuatAkun.classList.add('bg-secondary');
-
 	}else {
 		iconDarkMode = '<h4><i class="bi bi-moon-fill"></i></h4>';
 		localStorage.removeItem('theme');
-
-		// for (var i = 0; i <= navLink.length; i++) {
-		// 	navLink[i].classList.toggle('text-white');
-		// }
-
-		// btnUser.classList.remove('bg-secondary');
-
-		// btnMasuk.classList.remove('bg-secondary');
-		// btnMasuk.classList.add('bg-light');
-
-		// btnBuatAkun.classList.remove('bg-secondary');
-		// btnBuatAkun.classList.add('bg-light');
 
 	}
 	document.getElementById('darkBtn').innerHTML = iconDarkMode;
--- masuk/masuk.js
+++ masuk/masuk.js
@@ -1,5 +1,14 @@
-localStorage.setItem('user-name', 'admin');
-localStorage.setItem('pass-word', 'admin');
+// admin
+localStorage.setItem('user-admin', 'admin');
+localStorage.setItem('pass-admin', 'admin');
+
+// dosen
+localStorage.setItem('user-dosen', 'dosen');
+localStorage.setItem('pass-dosen', 'dosen');
+
+// mahasiswa
+localStorage.setItem('user-mhs', 'musmuliadi');
+localStorage.setItem('pass-mhs', 'sembarang');
 
 const formMasuk = document.querySelector('.form-masuk');
 const alertLogin = document.querySelector('.alert-login');
@@ -10,10 +19,16 @@ function login() {
 	const password = document.querySelector('#password');
 	const alertLogin = document.querySelector('.alert-login');
 
-	if(namaPengguna.value == localStorage.getItem('user-name') && password.value == localStorage.getItem('pass-word')){
+	if(namaPengguna.value == localStorage.getItem('user-admin') && password.value == localStorage.getItem('pass-admin')){
 
 		btnMasuk.setAttribute('href', '../dashboard/admin/graph/graph.html');
-		console.log(btnMasuk);
+
+	}else if(namaPengguna.value == localStorage.getItem('user-dosen') && password.value == localStorage.getItem('pass-dosen')){
+		btnMasuk.setAttribute('href', '../dashboard/users dosen/users-dosen.html');
+
+	}else if(namaPengguna.value == localStorage.getItem('user-mhs') && password.value == localStorage.getItem('pass-mhs')){
+		btnMasuk.setAttribute('href', '../dashboard/users mahasiswa/users-mhs.html');
+
 	}else {
 		alertLogin.classList.toggle('d-none');
 	}
